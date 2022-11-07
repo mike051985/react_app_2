7b2dfb0ef7ce6396a8fb7d8c4c06d0ade9f56a9d
Michelet Saint-Preux <michelet2000@gmail.com>
2022-11-07 20:55:53 +0800

    app.js components/Register.js style.css

--- app.js
+++ app.js
@@ -1,8 +1,43 @@
+// Reset component
+function Reset() {
+    return (
+    <div>
+        <h3>Reset Password</h3>
+    </div>
+  )
+}
 
+// Login component
+function Login() {
+    return (
+    <div>
+        <h3>Login</h3>
+    </div>
+  )
+}
+
+// Register component
+function Register() {
+    return (
+    <div>
+        <h3>Register</h3>
+    </div>
+  )
+}
+// App component
 function MyApp() {
-    return <h1>Hello</h1>;
+  
+    return (
+      <main className="App">
+        <Reset />
+        <Login />
+        <Register />
+      </main>
+    )
   }
 
   const container = document.getElementById('root');
   const root = ReactDOM.createRoot(container);
   root.render(<MyApp />);
+
+// Variables
